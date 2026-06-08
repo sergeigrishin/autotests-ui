276aabb909c88635b0c34446192af53111c8d994
Sergei <sergeigrishin.go@gmail.com>
2026-06-08 11:30:46 +0300

    Новый скрипт: Проверка формы регистрации

--- playwright_authorization.py
+++ playwright_authorization.py
@@ -1,20 +1,29 @@
 from playwright.sync_api import sync_playwright, expect
 
 with sync_playwright() as playwright:
-    chromium = playwright.chromium.launch(headless=False)
-    page = chromium.new_page()
+    # Открываем браузер и создаем новую страницу
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
 
+    # Переходим на страницу входа
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
-    email_input = page.locator('//div[@data-testid="login-form-email-input"]//input')
-    email_input.fill('user.name@gmail.com')
+    # Заполняем поле email
+    email_input = page.get_by_test_id('login-form-email-input').locator('input')
+    email_input.fill("user.name@gmail.com")
 
-    password_locator = page.locator('//div[@data-testid="login-form-password-input"]//input')
-    password_locator.fill('Password')
+    # Заполняем поле пароль
+    password_input = page.get_by_test_id('login-form-password-input').locator('input')
+    password_input.fill("password")
 
-    login_button = page.locator('//button[@data-testid="login-page-login-button"]')
+    # Нажимаем на кнопку Login
+    login_button = page.get_by_test_id('login-page-login-button')
     login_button.click()
 
-    wrong_email_or_password_alert = page.locator(' //div[@data-testid="login-page-wrong-email-or-password-alert"]')
+    # Проверяем, что появилось сообщение об ошибке
+    wrong_email_or_password_alert = page.get_by_test_id('login-page-wrong-email-or-password-alert')
     expect(wrong_email_or_password_alert).to_be_visible()
     expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
+
+    # Задержка для наглядности выполнения теста (не рекомендуется использовать в реальных автотестах)
+    page.wait_for_timeout(2000)
