f0e7374a2a1eec69bb991f8b2030cc4e1bb0c86a
Sergei <sergeigrishin.go@gmail.com>
2026-06-08 14:19:37 +0300

    Добавлено 2 файла: работа с клавиатурой, js

---
+++ playwright_keyboard.py
@@ -0,0 +1,18 @@
+from playwright.sync_api import sync_playwright, expect
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+
+    email_locator = page.get_by_test_id('login-form-email-input').locator('input')
+    email_locator.focus()
+
+    for char in "user@mail.ru":
+        page.keyboard.type(char)
+
+    page.keyboard.press("ControlOrMeta+A")
+
+
+    page.wait_for_timeout(5000)
