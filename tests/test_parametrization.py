f246e31ca543f6caebda58fbca3eff3fdbf252b5
Sergei <sergeigrishin.go@gmail.com>
2026-06-17 13:34:49 +0300

    Практика работы с pytest параметризацией

---
+++ tests/test_parametrization.py
@@ -0,0 +1,45 @@
+from idlelib.pyshell import idle_showwarning
+from tokenize import blank_re
+
+import pytest
+from playwright.sync_api import sync_playwright, expect
+
+
+@pytest.mark.parametrize('number', [1, 2, -1, 0])
+def test_number(number: int):
+    assert number > 0
+
+
+@pytest.fixture(params=['chromium', 'webkit', 'firefox'])
+def browser(request: SubRequest):
+    return request.param
+
+
+def test_open_browser(browser):
+    print(f'Running test on browser: {browser}')
+
+
+@pytest.mark.parametrize('user', ['Alice', 'Jo'])
+class TestOperations:
+
+    @pytest.mark.parametrize('account', ['Credit', 'Debet'])
+    def test_user_with_operations(self, user: str, account: str):
+        ...
+
+    def test_user_without_operations(self, user):
+        ...
+
+
+users = {
+    '911': 'user with money on bank account',
+    '01': 'user without money on bank account',
+    '88002000600': 'user operations on bank account'
+}
+
+@pytest.mark.parametrize(
+    'phone_number',
+    users.keys(),
+    ids=lambda phone_number: f'{phone_number}: {users[phone_number]}'
+)
+def test_identifiers(phone_number):
+    ...
