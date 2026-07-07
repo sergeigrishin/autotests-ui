979c619d68de7203355984edb7122a09f2173225
Sergei <sergeigrishin.go@gmail.com>
2026-07-07 11:08:18 +0300

    рефакторин

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -44,3 +44,37 @@ class TestCourses:
 
         courses_list_page.toolbar_view.check_visible()
         courses_list_page.check_visible_empty_view()
+
+
+    def test_edit_course(self, courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
+        create_course_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
+        create_course_page.image_upload_widget.upload_preview_image("./testdata/files/image.jpg")
+        create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
+        create_course_page.create_course_form.fill(
+            title="Playwright",
+            estimated_time="2 weeks",
+            description="Playwright",
+            max_score="100",
+            min_score="10"
+        )
+        create_course_page.create_course_toolbar_view.click_create_course_button()
+
+        courses_list_page.toolbar_view.check_visible()
+        courses_list_page.course_view.check_visible(
+            index=0, title="Playwright", max_score="100", min_score="10", estimated_time="2 weeks"
+        )
+        courses_list_page.course_view.menu.click_edit(index=0)
+
+        create_course_page.create_course_form.fill(
+            title="New Playwright",
+            estimated_time="3 weeks",
+            description="New Playwright",
+            max_score="1000",
+            min_score="100"
+        )
+        create_course_page.create_course_toolbar_view.click_create_course_button()
+
+        courses_list_page.toolbar_view.check_visible()
+        courses_list_page.course_view.check_visible(
+            index=0, title="New Playwright", max_score="1000", min_score="100", estimated_time="3 weeks"
+        )
