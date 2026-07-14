6f7da83270f39a5fa99bdfdab72fab54038ca460
Sergei <sergeigrishin.go@gmail.com>
2026-07-14 12:03:37 +0300

    def initialize_playwright_page(playwright:Playwright,

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,26 +1,24 @@
+import allure
 import pytest
 from playwright.sync_api import Playwright
-
+from _pytest.fixtures import SubRequest
+from tools.platwright.pages import initialize_playwright_page
 from pages.authentication.registration_page import RegistrationPage
-from pages.base_page import BasePage
 
 
 @pytest.fixture
-def chromium_page(playwright: Playwright):
-    browser = playwright.chromium.launch(headless=False)
-    yield browser.new_page()
-    browser.close()
+def chromium_page(playwright: Playwright, request: SubRequest):
+    yield from initialize_playwright_page(playwright, test_name=request.node.name)
 
 
 @pytest.fixture(scope='session')
 def initialize_browser_state(playwright: Playwright):
     browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
+    context = browser.new_context(record_video_dir='./videos')
     page = context.new_page()
 
     registration_page = RegistrationPage(page=page)
     registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
-
     registration_page.registration_form.fill(email='user@gmail.com', username='username', password='password')
     registration_page.click_registration_button()
 
@@ -29,8 +27,5 @@ def initialize_browser_state(playwright: Playwright):
 
 
 @pytest.fixture()
-def chromium_page_with_state(initialize_browser_state, playwright: Playwright):
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(storage_state="browser-state.json")
-    yield context.new_page()
-    browser.close()
+def chromium_page_with_state(request: SubRequest, initialize_browser_state, playwright: Playwright):
+    yield from initialize_playwright_page(playwright, test_name=request.node.name, storage_state="browser-state.json")
