8db2bb94a186ad826b88fba27f8aeba5c2c142cc
sergejgrisin <bermuddetut@gmail.com>
2026-07-23 16:03:24 +0300

    Запуск автотестов на GitHub Actions

--- conftest.py
+++ conftest.py
@@ -1,14 +1,6 @@
-import pytest
-
 pytest_plugins = (
     "fixtures.browsers",
     "fixtures.pages"
 )
 
 
-@pytest.fixture(scope="session")
-def browser_type_launch_args(browser_type_launch_args):
-    return {
-        **browser_type_launch_args,
-        "headless": True
-    }
